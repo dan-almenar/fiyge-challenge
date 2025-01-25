40a7c40e0030fa6f98c543cdde901725544a304d
Dan Almenar <danielalmenar@gmail.com>
2025-01-24 23:48:22 -0500

    UPDATE: basic handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 /* ANSI */
 const greenFont = '\x1b[38;5;10m';
-const redFont = '\x1b[38;5;10m';
+const redFont = '\x1b[38;5;9m';
 const boldFont = '\x1b[1m';
 const resetFont = '\x1b[0m';
 
@@ -11,12 +11,22 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 /* MIDDLEWARE */
+app.use(express.json());
 
-/* HANDLERS */
-
-
-app.listen(PORT, () => {
-	console.log(`${boldFont}${greenFont} Server listening in PORT: ${PORT}${resetFont}`);
+/* ROUTES */
+app.get('/api/forms/list', (req, res, next) => {
+	console.log(req.headers);
+	res.send('Hello World');
+	next();
 })
+
+try{
+	app.listen(PORT, () => {
+		console.log(`${boldFont}${greenFont} Server listening in PORT: ${PORT}${resetFont}`);
+	})
+} catch(err){
+	console.error(`${boldFont}${redFont}Server initialization error${resetFont}`, err);
+}
+
 
 /* SUPPORT FUNCTIONS */
