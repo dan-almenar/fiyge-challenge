c843cdce4a5f1e3c0c2b9834b0d8ea77afbe1a2a
Dan Almenar <danielalmenar@gmail.com>
2025-01-24 21:05:29 -0500

    ADD: basic server config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello World');
+import express from 'express';
+
+/* ANSI */
+const greenFont = '\x1b[38;5;10m';
+const redFont = '\x1b[38;5;10m';
+const boldFont = '\x1b[1m';
+const resetFont = '\x1b[0m';
+
+/* BASIC SERVER CONFIG */
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+/* MIDDLEWARE */
+
+/* HANDLERS */
+
+
+app.listen(PORT, () => {
+	console.log(`${boldFont}${greenFont} Server listening in PORT: ${PORT}${resetFont}`);
+})
+
+/* SUPPORT FUNCTIONS */
